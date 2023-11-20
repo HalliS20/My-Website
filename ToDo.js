92d07f467e6210acc8549622e9ceeb9528883926
HalliS <hallisteini@gmail.com>
2023-11-20 22:55:39 +0000

    added basic todo list

---
+++ ToDo.js
@@ -0,0 +1,15 @@
+let addToDoButton = document.getElementById("TodoButton");
+let toDoContainer = document.getElementById("TodoList");
+let inputField = document.getElementById("TodoInput");
+
+addToDoButton.addEventListener("click", function () {
+  var listItem = document.createElement("li");
+  listItem.innerText = inputField.value;
+  toDoContainer.appendChild(listItem);
+  listItem.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(listItem);
+  });
+  listItem.addEventListener("click", function () {
+    listItem.style.textDecoration = "line-through";
+  });
+});
