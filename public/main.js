90e03e95d8222de768cbdfe57eca9a44c3330dd8
HalliS2o <hallist@hey.com>
2023-11-27 12:40:09 +0000

    ordr of calls fixed

--- public/main.js
+++ public/main.js
@@ -1,13 +1,13 @@
-import { navbar } from "./scripts/navbar.js"
 import { loadPage } from "./scripts/loadPage.js"
 import { TodoList } from "./scripts/TodoList.js"
 import { projecter } from "./scripts/projecter.js"
+import { navbar } from "./scripts/navbar.js"
 
 document.addEventListener("DOMContentLoaded", function () {
     // Initial load of the navbar
+    window.TodoList = TodoList
     window.projecter = projecter
     window.loadPage = loadPage
-    window.TodoList = TodoList
     navbar()
     // Event listeners for each navigation link
 })
