16bd5010d55d894e05f29debaec98802937feef1
HalliS2o <hallist@hey.com>
2023-11-27 12:41:09 +0000

    ordr of calls fixed

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
-import { loadPage } from "./scripts/loadPage.js"
 import { TodoList } from "./scripts/TodoList.js"
 import { projecter } from "./scripts/projecter.js"
+import { loadPage } from "./scripts/loadPage.js"
 import { navbar } from "./scripts/navbar.js"
 
 document.addEventListener("DOMContentLoaded", function () {
