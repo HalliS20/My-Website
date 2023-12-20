c6bbb9d5c354ef42084e28cdd26d2ff419a636e1
HalliS2o <hallist@hey.com>
2023-12-20 11:08:51 +0000

    added comments to todo, animated navbar
    also added html plugin for prettier

--- public/scripts/Projecter.js
+++ public/scripts/Projecter.js
@@ -5,19 +5,21 @@ export function projecter() {
             document.getElementById("content").innerHTML = data
             const projects = document.getElementById("projects")
             ///////////////////// LIST OF PROJECTS /////////////////////
-            const projList = ["todoList"]
-            ///////////////////////////////////////////////////////////
+            const projList = ["todoList", "music"]
+            // Project Ideas
+            // Tic Tac Toe, Pomodoro Timer, Drum Machine
             for (const proj of projList) {
                 const projItem = document.createElement("li")
                 const projLink = document.createElement("a")
                 projLink.href = "#"
                 projLink.textContent = proj
                 projLink.addEventListener("click", () => {
+                    //================== Sets site content to project on link click ==================//
                     fetch(`../views/${proj}.html`).then((response) => {
                         response.text().then((data) => {
                             document.getElementById("content").innerHTML = data
                             if (proj === "todoList") {
-                                const todoList = new window.TodoList()
+                                new window.TodoList() //===== Make new TodoList JS object =====//
                             }
                         })
                     })
