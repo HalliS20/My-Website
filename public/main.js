b69f13c1590d04685aa594e7765236e436a813ba
Haraldur Steinar Skúlason <hallist@hey.com>
2024-02-08 09:17:20 +0000

    fixed typo

--- public/main.js
+++ public/main.js
@@ -1,8 +1,8 @@
 import { TodoList } from "./scripts/TodoList.js";
-import { projecter } from "./scripts/projecter.js";
 import { loadPage } from "./scripts/loadPage.js";
 import { navbar } from "./scripts/navbar.js";
 import { music } from "./scripts/music.js";
+import { projecter } from "./scripts/Projecter.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   // Initial load of the navbar
