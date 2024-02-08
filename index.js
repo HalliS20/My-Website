950186712506c3a31fa5ccb28d1cb1c19810fd6e
Haraldur Steinar Skúlason <hallist@hey.com>
2024-02-08 09:27:25 +0000

    fixed typo

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-import express from "express";
-import cors from "cors";
-import firebase from "firebase/compat/app";
-import "firebase/compat/storage";
+import express from "express"
+import cors from "cors"
+import firebase from "firebase/compat/app"
+import "firebase/compat/storage"
 
-const app = express();
-app.use(express.static("public"));
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(express.static("public"))
+app.use(cors())
+app.use(express.json())
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXUQo9B5V3oZ2j9nxc7lTex0ezU3BqSvc",
-  authDomain: "muser-11dba.firebaseapp.com",
-  projectId: "muser-11dba",
-  storageBucket: "muser-11dba.appspot.com",
-  messagingSenderId: "811128100318",
-  appId: "1:811128100318:web:338142903bdbce063ef94f",
-  measurementId: "G-Z1R0MTX35Z",
-};
+    apiKey: "AIzaSyBXUQo9B5V3oZ2j9nxc7lTex0ezU3BqSvc",
+    authDomain: "muser-11dba.firebaseapp.com",
+    projectId: "muser-11dba",
+    storageBucket: "muser-11dba.appspot.com",
+    messagingSenderId: "811128100318",
+    appId: "1:811128100318:web:338142903bdbce063ef94f",
+    measurementId: "G-Z1R0MTX35Z",
+}
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 /// get request to fetch the audio file from firebase storage
 app.get("/audio", async (req, res) => {
-  const storage = firebase.storage();
-  const storageRef = storage.ref();
-  const audioList = [];
+    const storage = firebase.storage()
+    const storageRef = storage.ref()
+    const audioList = []
 
-  storageRef
-    .listAll()
-    .then(async (result) => {
-      const promises = result.items.map(async (itemRef) => {
-        const url = await itemRef.getDownloadURL();
-        audioList.push({ name: itemRef.name, url: url });
-      });
+    storageRef
+        .listAll()
+        .then(async (result) => {
+            const promises = result.items.map(async (itemRef) => {
+                const url = await itemRef.getDownloadURL()
+                audioList.push({name: itemRef.name, url: url})
+            })
 
-      await Promise.all(promises);
+            await Promise.all(promises)
 
-      res.status(200).send(audioList);
-    })
-    .catch((error) => {
-      console.error("Error listing files:", error);
-    });
-});
+            res.status(200).send(audioList)
+        })
+        .catch((error) => {
+            console.error("Error listing files:", error)
+        })
+})
 
 app.get("/", (request, response) => {
-  response.send("Hi there");
-});
+    response.send("Hi there")
+})
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`)
+})
--- public/main.js
+++ public/main.js
@@ -1,15 +1,15 @@
-import { TodoList } from "./scripts/TodoList.js";
-import { loadPage } from "./scripts/loadPage.js";
-import { navbar } from "./scripts/navbar.js";
-import { music } from "./scripts/music.js";
-import { projecter } from "./scripts/Projecter.js";
+import {TodoList} from "./scripts/TodoList.js"
+import {loadPage} from "./scripts/loadPage.js"
+import {navbar} from "./scripts/navbar.js"
+import {music} from "./scripts/music.js"
+import {projecter} from "./scripts/projecter.js"
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Initial load of the navbar
-  window.TodoList = TodoList;
-  window.projecter = projecter;
-  window.loadPage = loadPage;
-  window.music = music;
-  navbar();
-  // Event listeners for each navigation link
-});
+    // Initial load of the navbar
+    window.TodoList = TodoList
+    window.projecter = projecter
+    window.loadPage = loadPage
+    window.music = music
+    navbar()
+    // Event listeners for each navigation link
+})
